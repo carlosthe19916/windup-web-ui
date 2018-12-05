a1b587ef7c646524d30c989fa86b9550b135b758
carlos <carlos@localhost.localdomain>
2018-12-05 13:32:50 -0500

    first template

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'windup-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'windup-web-ui';
+}
